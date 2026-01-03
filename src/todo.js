45d1b66513d6475f36892ff994720a22c2f8ce1d
jenaro fv <jen4r0@gmail.com>
2026-01-02 22:31:47 -0600

    implement private properties con Todo class

--- src/todo.js
+++ src/todo.js
@@ -1,8 +1,16 @@
 // Todo module
 
-import Projects from "./projects.js"
-
 class Todo {
+
+  #title;
+  #description;
+  #dueDate;
+  #priority;
+  #notes;
+  #checklist;
+  #done;
+  #projects;
+
   constructor (title, description, dueDate, prio, notes, checklist) {
     this.title = title;
     this.description = description;
@@ -15,39 +23,71 @@ class Todo {
   }
   
   set title (newTitle) {
-    this.title = newTitle;
+    this.#title = newTitle;
   }
 
   set description (newDescription) {
-    this.description = newDescription;
+    this.#description = newDescription;
   }
 
   set dueDate (newDueDate) {
-    this.dueDate = newDueDate;
+    this.#dueDate = newDueDate;
   }
 
   set priority (newPriority) {
-    this.priority = newPriority;
+    this.#priority = newPriority;
   }
 
   set notes (newNotes) {
-    this.notes = newNotes;
+    this.#notes = newNotes;
   }
 
   set checklist (newChecklist) {
-    this.checklist = newChecklist;
+    this.#checklist = newChecklist;
   }
 
   addToProject (project) {
-    // WIP
+    this.projects.add(project);
   }
 
-  deleteFromProject () {
-    // WIP
+  deleteFromProject (project) {
+    this.projects.delete(project);
   }
 
-  checkAsDone () {
-    this.done = true;
+  get title () {
+    return this.#title;
+  }
+
+  get description () {
+    return this.#description;
+  }
+
+  get dueDate () {
+    return this.#dueDate;
+  }
+
+  get priority () {
+    return this.#priority;
+  }
+
+  get notes () {
+    return this.#notes;
+  }
+
+  get checklist () {
+    return this.#checklist;
+  }
+
+  get done () {
+    return this.#done;
+  }
+
+  set done (bool) {
+    if (typeof bool === "boolean") {
+      this.#done = bool;
+    } else {
+      throw new Error ("property `done ` should be bool");
+    }
   }
 }
 
