70829cacd43f0566ec1d744ce386f000a9f7fa98
jenaro fv <jen4r0@gmail.com>
2026-01-02 22:28:27 -0600

    remove greeting.js and import Projects

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import diceImage from "./dice1.jpg";
 import "./style.css";
 import Todo from "./todo.js";
+import Projects from "./projects.js";
 
 const image = document.createElement("img");
 image.src = diceImage;
 image.width = "200";
 
 document.body.appendChild(image);
-
-console.log(greeting);
