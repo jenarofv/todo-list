c43d9b18ecd558f0317e890c684dbedf3a01e349
jenaro fv <jen4r0@gmail.com>
2026-01-03 16:26:45 -0600

    remove dice image taken from template

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-import diceImage from "./dice1.jpg";
 import "./style.css";
 import Todo from "./todo.js";
 import Projects from "./projects.js";
-
-const image = document.createElement("img");
-image.src = diceImage;
-image.width = "200";
-
-document.body.appendChild(image);
