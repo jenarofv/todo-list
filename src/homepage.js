1a0673b1813019a935b6dc20588e08cc62358d6c
jenaro fv <jen4r0@gmail.com>
2026-01-04 07:10:31 -0600

    add todo container and add-todo button

--- src/homepage.js
+++ src/homepage.js
@@ -1,6 +1,13 @@
 export default function () {
   const body = document.querySelector("body");
   const title = document.createElement("h1");
+  const main = document.createElement("main");
+  const addBtn = document.createElement("button");
+  addBtn.id = "add-todo";
+  addBtn.textContent = "+";
+  main.classList.add("container");
   title.innerText = "Todo List";
   body.appendChild(title);
+  body.appendChild(main);
+  body.appendChild(addBtn);
 }
